f2175f720d686b971f7edfa2230c018badee3431
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-06 09:33:42 +0530

    Add files via upload

---
+++ multipleof8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class multipleof8{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            if((n&7)==0)
+            {
+                System.out.println("multiple of 8");
+            }
+            else
+            {
+                System.out.println("not a multiple of 8");
+            }
+        }
+    }
+}
