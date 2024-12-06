38efe8d3a00306bfe6f9c4d26d68dddc6ce185cf
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-06 11:48:58 +0530

    Add files via upload

---
+++ forloopeven.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class forloopeven{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int i=sc.nextInt();
+    for(i=0;i<50;i+=2){
+        System.out.println(i);
+    }
+}
+}
